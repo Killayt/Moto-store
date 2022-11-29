67706d6f612a5b98806b44f623f1b267081aaa96
Killayt <surnamename615@gmail.com>
2022-11-29 15:01:40 +0300

    Added base template

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -9,76 +9,71 @@ import (
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("web/templates/index.html")
-	if err != nil {
-		fmt.Println("Error parse templates\n\n", err.Error())
-	}
-
 	homePage := model.Page{
 		Title:   "Home",
 		Text:    "hueta",
 		Product: model.Product{},
 	}
 
+	tmpl, err := template.ParseFiles("web/templates/index.html")
+	if err != nil {
+		fmt.Println("Error parse templates\n\n", err.Error())
+	}
 	tmpl.Execute(w, homePage)
 }
 
 func AboutUs(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("web/templates/about.html")
-	if err != nil {
-		fmt.Println("Error parse templates\n\n", err.Error())
-	}
-
 	aboutPage := model.Page{
 		Title:   "About us",
 		Text:    "hueta",
 		Product: model.Product{},
 	}
 
+	tmpl, err := template.ParseFiles("web/templates/about.html")
+	if err != nil {
+		fmt.Println("Error parse templates\n\n", err.Error())
+	}
 	tmpl.Execute(w, aboutPage)
 }
 
 func Contact(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("web/templates/contact.html")
-	if err != nil {
-		fmt.Println("Error parse templates\n\n", err.Error())
-	}
-
 	contactPage := model.Page{
 		Title:   "Contact",
 		Text:    "hueta",
 		Product: model.Product{},
 	}
 
+	tmpl, err := template.ParseFiles("web/templates/contact.html")
+	if err != nil {
+		fmt.Println("Error parse templates\n\n", err.Error())
+	}
 	tmpl.Execute(w, contactPage)
 }
 
 func Payment(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("web/templates/payment.html")
-	if err != nil {
-		fmt.Println("Error parse templates\n\n", err.Error())
-	}
-
 	paymentPage := model.Page{
 		Title:   "Payment",
 		Text:    "hueta",
 		Product: model.Product{},
 	}
 
+	tmpl, err := template.ParseFiles("web/templates/payment.html")
+	if err != nil {
+		fmt.Println("Error parse templates\n\n", err.Error())
+	}
 	tmpl.Execute(w, paymentPage)
 }
 
 func Catalog(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("web/templates/catalog.html")
-	if err != nil {
-		fmt.Println("Error parse templates\n\n", err.Error())
-	}
-
 	catalogPage := model.Page{
 		Title:   "Catalog",
 		Text:    "hueta",
 		Product: model.Product{},
 	}
 
+	tmpl, err := template.ParseFiles("web/templates/catalog.html")
+	if err != nil {
+		fmt.Println("Error parse templates\n\n", err.Error())
+	}
 	tmpl.Execute(w, catalogPage)
 }
